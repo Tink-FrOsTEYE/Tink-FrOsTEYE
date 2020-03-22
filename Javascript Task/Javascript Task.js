9dde66af59492feff54965963375846bd44f1f18
Tink-FrOsTEYE <52077615+Tink-FrOsTEYE@users.noreply.github.com>
2020-03-22 17:17:54 +0100

    Create Javascript Task.js

---
+++ Javascript Task/Javascript Task.js
@@ -0,0 +1,22 @@
+var myName = "Adeyeye Olalekan";
+
+var courses = ["HTML", "CSS", "Javascript"];
+
+console.log(myName +"\'s courses:");
+
+for(element of courses){
+    console.log(element);
+}
+
+
+//using courses.length returns 3 which is an odd number.
+
+for(var i=1; i<=200; i++){
+    if (i%2 != 0){
+
+//using i%2 == 0 returns even number while i%2 !=0 returns odd number.
+
+        console.log(i);
+
+    }
+    }
